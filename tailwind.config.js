987da37dd74b0e331f7d8f223a67d25434523842
Daniel Bannert <prisis@users.noreply.github.com>
2022-06-02 15:29:10 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                "noto-sans": ["Nunito Sans", "Helvetica Neue", "Helvetica", "Arial", "Noto Sans SC", "sans-serif"],
+            },
+        },
+    },
+    plugins: [],
+};
